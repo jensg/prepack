fc9fba21b9b56b4e2cb732d6468782de34684f40
Nikolai Tillmann <nikolait@fb.com>
2017-06-02 17:49:55 -0700

    Keep track of the scopes (in terms of functions) in which values are needed.

--- src/serializer/serializer.js
+++ src/serializer/serializer.js
@@ -1709,7 +1709,7 @@ export class Serializer {
     residualHeapVisitor.visitRoots();
     if (this.logger.hasErrors()) return undefined;
     this.ignoredProperties = residualHeapVisitor.ignoredProperties;
-    this.residualValues = residualHeapVisitor.values;
+    this.residualValues = new Set(residualHeapVisitor.values.keys());
     this.residualFunctionBindings = residualHeapVisitor.functionBindings;
     this.residualFunctionInfos = residualHeapVisitor.functionInfos;
 
